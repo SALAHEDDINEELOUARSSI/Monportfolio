2545d686265fe16cbb1f5506add495f08788ba41
SALAHEDDINEELOUARSSI <salaheddine.elouarssi23@ump.ac.ma>
2025-09-17 17:01:33 +0100

    portfolio

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 
 // Charger AnimationLottie uniquement côté client
 const AnimationLottie = dynamic(
-    () => import("./AnimationLottie"),
+    () => import("./animationLottie"),
     { ssr: false } // ❌ Pas de rendu côté serveur
 );
 
